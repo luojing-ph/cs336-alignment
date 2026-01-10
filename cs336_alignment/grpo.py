7755e0b19c2aa501c5c4d886a4364c6d132a0d7f
luojing-ph <luojing4c@gmail.com>
2026-01-09 21:31:40 -0800

    add grpo_microbatch_train_step

--- cs336_alignment/grpo.py
+++ cs336_alignment/grpo.py
@@ -1,5 +1,6 @@
 from importlib.metadata import metadata
 from typing import Callable, Literal
+from urllib import response
 
 import einops
 import torch
@@ -100,3 +101,29 @@ def compute_policy_gradient_loss(
 def masked_mean(tensor: torch.Tensor, mask: torch.Tensor, dim: int | None = None) -> torch.Tensor:
     masked_tensor = tensor * mask
     return torch.sum(masked_tensor, dim=dim) / torch.sum(mask, dim=dim)
+
+
+def grpo_microbatch_train_step(
+        policy_log_probs: torch.Tensor,
+        response_mask: torch.Tensor,
+        gradient_accumulation_steps: int,
+        loss_type: Literal["no_baseline", "reinforce_with_baseline", "grpo_clip"],
+        raw_rewards: torch.Tensor | None = None,
+        advantages: torch.Tensor | None = None,
+        old_log_probs: torch.Tensor | None = None,
+        cliprange: float | None = None,
+) -> tuple[float, dict[str, torch.Tensor]]:
+    loss, metadata = compute_policy_gradient_loss(
+        policy_log_probs=policy_log_probs,
+        loss_type=loss_type,
+        raw_rewards=raw_rewards,
+        advantages=advantages,
+        old_log_probs=old_log_probs,
+        cliprange=cliprange,
+    )
+
+    loss = masked_mean(loss, response_mask)
+    loss = loss / gradient_accumulation_steps
+    loss.backward()
+
+    return loss, metadata
--- tests/adapters.py
+++ tests/adapters.py
@@ -251,7 +251,8 @@ def run_grpo_microbatch_train_step(
         tuple[torch.Tensor, dict[str, torch.Tensor]]: 
             the policy gradient loss and its metadata.
     """
-    raise NotImplementedError
+    return grpo_microbatch_train_step(policy_log_probs, response_mask, gradient_accumulation_steps, loss_type,
+                                      raw_rewards, advantages, old_log_probs, cliprange)
 
 
 def run_masked_normalize(
